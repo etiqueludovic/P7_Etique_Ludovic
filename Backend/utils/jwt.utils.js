6172e38ecd82d8130eae8933a88d0bbaae1b4697
etiqueludovic <etique.ludovic@gmail.com>
2022-01-18 10:51:10 +0100

    Create jwt.utils.js

---
+++ Backend/utils/jwt.utils.js
@@ -0,0 +1,36 @@
+// import
+var jwt = require('jsonwebtoken');
+
+// construction du token
+const JWT_SIGN_SECRET = '<JWT_SIGN_TOKEN>';
+
+// On exporte la function pour générer un token pour l'utilisateur
+module.exports = {
+    generateTokenForUser: function(userData) {
+        return jwt.sign({
+            userId: userData.id,
+            isAdmin: userData.isAdmin
+        },
+            JWT_SIGN_SECRET,
+        {
+            expiresIn: '1h'
+        })
+    },
+    parseAuthorization: function (authorization) {
+        return (authorization != null) ? authorization.replace('Bearer ', '') : null;
+    },
+    getUserId: function(authorization) {
+        var userId = -1;
+        var token = module.exports.parseAuthorization(authorization);
+        if(token != null) {
+            try {
+                var jwtToken = jwt.verify(token, JWT_SIGN_SECRET);
+                if(jwtToken != null)
+                userId = jwtToken.userId;
+            } catch(err) {
+
+            }
+        } 
+        return userId;
+    }
+}
