c24440b1c33137ebbc737af2ffaef812fa5c08bc
etiqueludovic <etique.ludovic@gmail.com>
2022-01-18 10:50:40 +0100

    Create 20211122093524-create-message.js

---
+++ Backend/migrations/20211122093524-create-message.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('messages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id'
+        }
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      imageUrl: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      CreatAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      UpdatAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('messages');
+  }
+};
