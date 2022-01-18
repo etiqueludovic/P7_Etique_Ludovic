2ad82e894caa5d1140faa6a03047dc49a6741750
etiqueludovic <etique.ludovic@gmail.com>
2022-01-18 10:49:37 +0100

    Delete indexControllers.js

--- Backend/app/controllers/indexControllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class IndexControllers {
-    index(req, res) {
-        res.json({ text: 'API works Successfully!!! /' });
-    }
-}
-exports.indexController = new IndexControllers();
-//# sourceMappingURL=indexControllers.js.map
