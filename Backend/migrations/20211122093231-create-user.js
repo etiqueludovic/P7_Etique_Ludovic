225487dfe638a88ab90fe16a6c71c36db46a9e12
etiqueludovic <etique.ludovic@gmail.com>
2022-01-18 10:50:35 +0100

    Create 20211122093231-create-user.js

---
+++ Backend/migrations/20211122093231-create-user.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('user', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      username: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      bio: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      profil_image: {
+        allownull: true,
+        type: Sequelize.STRING
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('user');
+  }
+};
