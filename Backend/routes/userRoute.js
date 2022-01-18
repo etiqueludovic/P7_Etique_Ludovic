17e0dcfcde45d34d41c879c7b5186bdf0e9820f4
etiqueludovic <etique.ludovic@gmail.com>
2022-01-18 10:51:04 +0100

    Create userRoute.js

---
+++ Backend/routes/userRoute.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/userCtrl');
+const validate = require('../middleware/validate');
+const auth = require('../middleware/auth');
+
+router.post('/register', validate.newUser, userCtrl.newuser);
+router.post('/login', validate.login, userCtrl.login);
+router.get('/profil/:userId', userCtrl.getProfil);
+router.get('/list', auth, userCtrl.getAllUsers);
+router.get('/:userId', auth, validate.id, userCtrl.getOneUser);
+router.get('/:userId/message', auth, validate.id, userCtrl.getAllPostsOfUser);
+router.put('/:userId/username', auth, userCtrl.changeusername);
+router.put('/:userId/password', auth, validate.changePassword, userCtrl.changePassword);
+router.put('/:userId/profil_image', auth, userCtrl.changeProfilePicture);
+router.put('/:userId/bio', auth, userCtrl.changebio);
+router.delete('/:userId', auth, validate.id, userCtrl.deleteAccount);
+
+module.exports = router;
