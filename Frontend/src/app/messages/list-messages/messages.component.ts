6b8cccfcdaa1c32ab526ff092c0571d6b25bb043
etiqueludovic <etique.ludovic@gmail.com>
2022-01-22 09:03:19 +0100

    Update messages.component.ts

--- Frontend/src/app/messages/list-messages/messages.component.ts
+++ Frontend/src/app/messages/list-messages/messages.component.ts
@@ -4,13 +4,11 @@ import { Router, ActivatedRoute } from "@angular/router";
 import { AuthUser } from '../../services/auth.service';
 import { HttpClient, JsonpClientBackend } from '@angular/common/http';
 import { DomSanitizer } from '@angular/platform-browser';
-import { timer } from 'rxjs';
-import { NgIfContext } from '@angular/common';
 
-let id = ''; 
+let id = '';
 console.log("Ici ce trouve le token avant log")
 if (sessionStorage['token']){
-    id = JSON.parse(sessionStorage['token']).userId
+    id = JSON.parse(sessionStorage['token']).userId;
 }
 
 @Component({
@@ -20,6 +18,7 @@ if (sessionStorage['token']){
 })
 export class MessagesComponent implements OnInit {
 Posts:any=[];
+UserList: any=[];
 username!: string;
 authStatus!: boolean;
 dataItem!: number;
@@ -31,11 +30,17 @@ retrievedImage: any;
 index: any;
 image=[];
 
+retrieveResponse: any;
+base64DataProfil: any;
+retrievedImageprofil: any;
+
   constructor(private login: AuthUser, private router: Router, private service: AuthMessage, private activateroute: ActivatedRoute, private http: HttpClient, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
    this.authStatus = this.login.auth;
+   if (id) {
    this.ListMessages();
+  }
   }
 
   onCreate() {
@@ -45,16 +50,13 @@ image=[];
     }
 
   ListMessages() {
-    
     this.service.viewMessages().subscribe(data => {
-      
-      console.log(data)
       this.Posts=data;
       this.dataItem = this.Posts.id;
-      this.ChargeImage();     
-          })
-        }
-
+      this.getImage();  
+      this.ChargeImage();       
+      })
+    }
 
   ChargeImage() {
     for(let i = 0; i < this.Posts.length; i++) {
@@ -63,10 +65,10 @@ image=[];
             res => {
               this.retrieveResonse = res;
               this.base64Data = URL.createObjectURL(this.retrieveResonse);
-              this.retrievedImage = this.sanitizer.bypassSecurityTrustUrl(this.base64Data);
-            })
-          }
+              this.Posts[i].retrievedImage = this.sanitizer.bypassSecurityTrustUrl(this.base64Data); 
+          })
         }
+      }
 
   DeleteMessage() {
    this.activateroute.params.subscribe(data => {
@@ -75,9 +77,21 @@ image=[];
    })
    if (this.messageId)
    this.service.deleteMessage(this.messageId).subscribe(data => {
-     console.log("message supprimé numéro :"+this.messageId)
-     console.log(data)
    })
   }
-}
 
+  getImage() {
+    for(let j = 0; j < this.Posts.length; j++) {
+    this.http.get(this.Posts[j].profil_image, {responseType: 'blob'})
+      .subscribe(
+        res => {
+          this.retrieveResponse = res;
+          this.base64DataProfil = URL.createObjectURL(this.retrieveResponse);
+          this.Posts[j].retrievedImageprofil = this.sanitizer.bypassSecurityTrustUrl(this.base64DataProfil);
+        })
+      }
+    }
+
+
+    
+  }
