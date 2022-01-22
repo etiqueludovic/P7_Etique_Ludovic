4ed7a0c61006e038890720bcc4eb0470698b5ea9
etiqueludovic <etique.ludovic@gmail.com>
2022-01-22 09:03:50 +0100

    Update messageRoute.js

--- Backend/routes/messageRoute.js
+++ Backend/routes/messageRoute.js
@@ -7,7 +7,9 @@ const postCtrl = require('../controllers/messages');
 router.post('/addmessage', postCtrl.newMessages);
 router.get('/', postCtrl.getMessages);
 router.get('/:id', postCtrl.getOneMessages);
-router.put('/update/:id', postCtrl.updateMessages);
+router.put('/update/:id/content', postCtrl.updateContentMessages);
+router.put('/update/:id/title', postCtrl.updateTitleMessages)
+router.put('/update/:id/file', postCtrl.updateFileMessages)
 router.delete('/delete/:id', postCtrl.deleteMessage);
 
 module.exports = router;
