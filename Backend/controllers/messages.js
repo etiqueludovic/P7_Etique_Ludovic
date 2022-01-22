78a52c0714b8ae7c804a02bd3f92c9210f530170
etiqueludovic <etique.ludovic@gmail.com>
2022-01-22 09:02:09 +0100

    Update messages.js

--- Backend/controllers/messages.js
+++ Backend/controllers/messages.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-const { date } = require('joi');
 const database = require('../utils/database');
 
 const connection = database.connect();
@@ -9,21 +8,24 @@ const connection = database.connect();
  * Ajout d'une nouvelle publication
  */
 exports.newMessages = (req, res, next) => {
-  let originalname = req.body.imageUrl;
-
-  let ext = originalname.split('.').pop();
-  let filename = originalname.split('.').slice(0, -1).join('.');
+  let originalname = req.body.ImageUrl;
+  console.log("ici le nom original"+originalname)
+  let ext = originalname?.split('.').pop();
+  let filename = originalname?.split('.').slice(0, -1).join('.');
+  let date = Math.floor(Date.now() /1000);
+  
   const newMessage = {
     userId: req.body.userId,
-    imageurl: filename + '-' + Date.now()+'.'+ext,
+    imageurl: filename + '-' + date +'.'+ext,
     content: req.body.content,
     title: req.body.title,
     username: req.body.username,
+    CreatAt: new Date()
   }
 
   connection.query("INSERT INTO messages SET ?", [newMessage], (error, results, fields) => {
     if (error) {
-      res.status(500).json({ "error": error.sqlMessage });
+      res.status(500).json({ "error": error.sqlMessage, message: newMessage });
     } else {
       res.status(200).json({ message: 'Publication ajoutée' });
     }
@@ -40,7 +42,8 @@ exports.getMessages = (req, res, next) => {
   }
 
 exports.deleteMessage = (req, res, next) => {
-  connection.query('DELETE FROM messages WHERE id= ?', [req.params.id], (err, rows, fields) => {
+  const postId = req.body.postId;
+  connection.query('DELETE FROM messages WHERE id = ?', [postId], (err, rows, fields) => {
     if(!err)
     res.send('Supprimé avec succès !');
     else
@@ -49,6 +52,7 @@ exports.deleteMessage = (req, res, next) => {
   }
 
 exports.getOneMessages = (req, res, next) => {
+  const postId = req.body.postId
   connection.query('SELECT * FROM messages WHERE id= ?', [req.params.id], (err, rows, fields) => {
     if(!err)
     res.send(rows);
@@ -57,23 +61,40 @@ exports.getOneMessages = (req, res, next) => {
     })
   }
 
-exports.updateMessages = (req, res, next) => {
-    const newMessage = {
-      userId: req.body.userId,
-      imageurl: req.body.ImageUrl,
-      content: req.body.content,
-      title: req.body.title,
-      username: req.body.username
-    }
-  
-    connection.query('UPDATE INTO messages SET ? WHERE id= ?', [newMessage], [req.params.id], (error, results, fields) => {
+exports.updateTitleMessages = (req, res, next) => {
+      const id = req.body.postId;
+      const title = req.body.title;
+    connection.query('UPDATE messages SET title=? WHERE id= ?', [title, id], (error, results, fields) => {
       if (error) {
         res.status(500).json({ "error": error.sqlMessage });
       } else {
-        res.status(200).json({ message: 'Publication mise à jour' });
+        res.status(200).json({ message: 'Titre mise à jour' });
       }
     });
-    connection.end();
+  }
+
+  exports.updateContentMessages = (req, res, next) => {
+    const id = req.body.postId;
+    const content = req.body.content;
+    connection.query('UPDATE messages SET title=? WHERE id= ?', [content, id], (error, results, fields) => {
+      if (error) {
+        res.status(500).json({ "error": error.sqlMessage });
+      } else {
+        res.status(200).json({ message: 'Content mise à jour' });
+      }
+    });
+  }
+
+  exports.updateFileMessages = (req, res, next) => {
+    const id = req.body.postId;
+    const imageUrl = req.body.imageUrl;
+    connection.query('UPDATE messages SET imageUrl=? WHERE id= ?', [imageUrl, id], (error, results, fields) => {
+      if (error) {
+        res.status(500).json({ "error": error.sqlMessage });
+      } else {
+        res.status(200).json({ message: 'Content mise à jour' });
+      }
+    });
   }
 
 
