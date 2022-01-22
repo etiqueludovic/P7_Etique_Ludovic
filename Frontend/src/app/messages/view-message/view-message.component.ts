c272d6a93e3b7f502e8ad802abbd3eba545cddf6
etiqueludovic <etique.ludovic@gmail.com>
2022-01-22 09:02:50 +0100

    Update view-message.component.ts

--- Frontend/src/app/messages/view-message/view-message.component.ts
+++ Frontend/src/app/messages/view-message/view-message.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthMessage } from 'src/app/services/message.service';
+import { UserService } from 'src/app/services/user.service';
 
 let username = ''; 
 if (sessionStorage['token']){
@@ -16,24 +17,24 @@ if (sessionStorage['token']){
 })
 export class ViewMessageComponent implements OnInit {
   Post: any=[];
-  userId: string = '';
+  User: any=[];
+  postId: string = '';
   imageName: any;
   retrieveResonse: any;
   base64Data: any;
   retrievedImage: any;
+  retrievedImageProfil: any;
   
 
-  constructor(private activedRoute: ActivatedRoute, private msgService: AuthMessage, private router: Router,  private http: HttpClient, private sanitizer: DomSanitizer) { }
+  constructor(private activedRoute: ActivatedRoute, private msgService: AuthMessage, private router: Router,  private http: HttpClient, private sanitizer: DomSanitizer, private user: UserService) { }
   cheminImage!: any;
   ngOnInit(): void {
 
     this.activedRoute.params.subscribe(data => {
-      this.userId = data['id'];
-      console.log("ici l'ID du message :")
-      console.log(this.userId);
+      this.postId = data['id'];
     });
    
-    this.msgService.ViewOneMessage(this.userId).subscribe(data => {
+    this.msgService.ViewOneMessage(this.postId).subscribe(data => {
       this.Post = data;
       this.http.get('http://localhost:3000/images/' + this.Post[0].imageUrl, {responseType: 'blob'})
           .subscribe(
@@ -42,26 +43,32 @@ export class ViewMessageComponent implements OnInit {
               this.base64Data = URL.createObjectURL(this.retrieveResonse);
               this.retrievedImage = this.sanitizer.bypassSecurityTrustUrl(this.base64Data);
             })
-        console.log(this.cheminImage)
      });
+     this.getImage();
   }
 
   DeleteMessage(){
-      if (this.userId == JSON.parse(sessionStorage['token']).id){
-      this.msgService.deleteMessage(this.userId).subscribe(data => {
+      if (this.postId == JSON.parse(sessionStorage['token']).id){
+      this.msgService.deleteMessage(this.postId).subscribe(data => {
       this.router.navigate(['/delete']);
       });
     }
   }
 
   getImage() {
-        //Make a call to Sprinf Boot to get the Image Bytes.
-        this.http.get('http://localhost:3000/images/' + this.imageName, {responseType: 'blob'})
+    this.user.getUserList().subscribe(data => {
+      this.User = data;
+      console.log("ici c'est le bordel")
+      let id = this.Post[0].userId;
+      const URL = this.User[id].profil_image;
+      console.log("ici l'url"+URL)
+        this.http.get(URL, {responseType: 'blob'})
           .subscribe(
             res => {
               this.retrieveResonse = res;
               this.base64Data = URL.createObjectURL(this.retrieveResonse);
-              this.retrievedImage = this.sanitizer.bypassSecurityTrustUrl(this.base64Data);
+              this.retrievedImageProfil = this.sanitizer.bypassSecurityTrustUrl(this.base64Data);
             })
-          }
+          })
+      }
 }
