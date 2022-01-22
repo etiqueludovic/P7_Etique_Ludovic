cea07509c54c871fc86f308d41bce448c46e119e
etiqueludovic <etique.ludovic@gmail.com>
2022-01-22 09:02:44 +0100

    Update update-message.component.ts

--- Frontend/src/app/messages/update-message/update-message.component.ts
+++ Frontend/src/app/messages/update-message/update-message.component.ts
@@ -4,6 +4,7 @@ import { AuthMessage } from '../../services/message.service';
 import { Router } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
 import { MatSnackBar } from '@angular/material/snack-bar'
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-update-message',
@@ -22,13 +23,46 @@ export class UpdateMessageComponent implements OnInit {
   previewUrl:any = null;
   fileUploadProgress!: string;
   uploadedFilePath!: string;
+  modif_t!: boolean;
+  modif_c!: boolean;
+  dataItem: any;
+  retrieveResponse: any;
+  base64DataProfil: any;
+  retrievedImageprofil: any;
   constructor(private router: Router,
               private http: HttpClient,
-              private authMessage: AuthMessage,
-              private _snackBar: MatSnackBar
+              private service: AuthMessage,
+              private _snackBar: MatSnackBar,
+              private sanitizer: DomSanitizer
               ) { }
 
   ngOnInit(): void {
+    this.ListMessages();
+  }
+
+  ListMessages() {
+    
+    const str = window.location.pathname
+    const num = str.charAt(str.length-1)
+    console.log(num)
+      this.service.ViewOneMessage(num).subscribe(data => {
+      this.Posts=data;
+      this.dataItem = this.Posts.id;    
+      this.getImage();   
+      })
+    }
+
+  UpdateOn_t() {
+    this.modif_t = true
+  }
+  UpdateOn_c() {
+    this.modif_c = true
+  }
+  Cancel_t() {
+    this.modif_t = false;
+  }
+  Cancel_c() {
+    this.modif_c = false;
   }
 
   fileProgress(fileInput: any) {
@@ -57,30 +91,83 @@ export class UpdateMessageComponent implements OnInit {
     }
   }
 
+  titre = this.title;
+  contenent = this.content;
+
   onSubmit(form: NgForm) {
-    const formulaire = {
+    const str = window.location.pathname
+    const num = str.charAt(str.length-1)
+    if (form.value.title) {
+    const formulaireTitle = {
       'title': form.value.title,
-      'content': form.value.content,
-      'ImageUrl': this.fileData.name,
-      'userId': JSON.parse(sessionStorage['token']).userId,
-      'username': JSON.parse(sessionStorage['token']).username
+      'postId': num,
     }
     const id = JSON.parse(sessionStorage['token']).userId;
-    console.log(formulaire)
-    const fd = new FormData();
-    fd.append('file', this.fileData, this.fileData.name)
-    this.http.post('http://localhost:3000/images', fd)
-    .subscribe(res => {
-      console.log(res)
-    })
-    this.authMessage.updateMessage(id, formulaire)
+    this.service.updateTitleMessage(id, formulaireTitle)
     .subscribe(() => {
-      console.log('Enregistré !');
-      this._snackBar.open("Message créé avec succès !")
-      console.log(formulaire);
-      this.router.navigate(['/messages']);
+      console.log('Titre mis à jour !');
+    })
+    if (form.value.content){
+      const formulaireContent = {
+        'title': form.value.title,
+        'postId': num,
+      }
+      const id = JSON.parse(sessionStorage['token']).userId;
+      this.service.updateContentMessage(id, formulaireContent)
+      .subscribe(() => {
+        console.log('Titre mis à jour !');
       })
-    
+    }
+    if (this.fileData.name){
+      const formulaireContent = {
+        'imageUrl': this.fileData.name,
+        'postId': num,
+      }
+      const id = JSON.parse(sessionStorage['token']).userId;
+      this.service.updateContentMessage(id, formulaireContent)
+      .subscribe(() => {
+        console.log('Titre mis à jour !');
+      })
+    }
   }
+}
+
+
+  showEmojiPicker = false;
+  toggleEmojiPicker() {
+    console.log(this.showEmojiPicker);
+        this.showEmojiPicker = !this.showEmojiPicker;
+  }
+
+  addEmoji(event: { emoji: { native: any; }; }) {
+    console.log(this.content)
+    const { content } = this;
+    console.log(content);
+    console.log(`${event.emoji.native}`)
+    const text = `${content}${event.emoji.native}`;
+
+    this.content = text;
+    // this.showEmojiPicker = false;
+  }
+
+  onFocus() {
+    console.log('focus');
+    this.showEmojiPicker = false;
+  }
+  onBlur() {
+    console.log('onblur')
+  }
+
+  getImage() {
+    for(let j = 0; j < this.Posts.length; j++) {
+    this.http.get(this.Posts[j].profil_image, {responseType: 'blob'})
+      .subscribe(
+        res => {
+          this.retrieveResponse = res;
+          this.base64DataProfil = URL.createObjectURL(this.retrieveResponse);
+          this.Posts[j].retrievedImageprofil = this.sanitizer.bypassSecurityTrustUrl(this.base64DataProfil);
+        })
+      }
+    }
 
 }
