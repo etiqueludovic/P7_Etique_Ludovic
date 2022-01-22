52623a32829e037a2c212276dabbf37b067fb663
etiqueludovic <etique.ludovic@gmail.com>
2022-01-22 16:26:48 +0100

    Update commentaireRoute.js

--- Backend/routes/commentaireRoute.js
+++ Backend/routes/commentaireRoute.js
@@ -8,6 +8,7 @@ const validate = require('../middleware/validate');
 
 router.post('/addcomment', auth, validate.comment, commentCtrl.newComment);
 router.get('/:id', auth, commentCtrl.getCommentsofPost);
+router.get('/', auth, commentCtrl.getCommentAllPosts);
 router.delete('/delete/:id', auth, validate.id, commentCtrl.deleteComment);
 
 module.exports = router;
