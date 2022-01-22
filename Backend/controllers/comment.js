c8617290185f082d669546ab48dc58b034d5d719
etiqueludovic <etique.ludovic@gmail.com>
2022-01-22 09:02:06 +0100

    Update comment.js

--- Backend/controllers/comment.js
+++ Backend/controllers/comment.js
@@ -7,25 +7,19 @@ exports.newComment = (req, res, next) => {
   const connection = database.connect();
 
   const newComment = {
-    userId: req.body.userId,
-    postId: req.body.postId,
-    content: req.body.content
+    user_id: req.body.user_id,
+    post_id: req.body.post_id,
+    content: req.body.content,
+    CreatAt: new Date()
   }
 
-  connection.execute("INSERT INTO Comments SET ?", [newComment], (error, results, fields) => {
+  connection.query("INSERT INTO Comments SET ?", [newComment], (error, results, fields) => {
     if (error) {
       res.status(500).json({ "error": error.sqlMessage });
     } else {
-      comment.addComment(userId, postId)
-        .then(data => {
-          res.status(201).json({ message: 'Commentaire ajoutée' });
-        })
-        .catch(err => {
-          res.status(500).json({ "error": err });
-        })
+      res.status(201).json({ message: 'Commentaire ajoutée' });
     }
   });
-  connection.end();
 }
 
 /**
@@ -33,14 +27,12 @@ exports.newComment = (req, res, next) => {
  */
 exports.getCommentsofPost = (req, res, next) => {
   const connection = database.connect();
-
-  const postId = req.body.postId;
-
-  connection.execute("SELECT * FROM Comments INNER JOIN user ON Comments.user_id = user.id WHERE Comments.post_id = ?", [postId], (error, comments, fields) => {
+  const id = req.params.id;
+  connection.query("SELECT * FROM Comments WHERE post_id=?", [id], (error, comments, fields) => {
     if (error) {
       res.status(500).json({ "error": error.sqlMessage });
     } else {
-      res.status(200).json({ comments });
+      res.status(200).json(comments);
     }
   });
 
@@ -53,12 +45,11 @@ exports.getCommentsofPost = (req, res, next) => {
 exports.deleteComment = (req, res, next) => {
   const connection = database.connect();
   const commentId = parseInt(req.params.id, 10);
-  connection.execute("DELETE FROM Comments WHERE id=?", [commentId], (error, results, fields) => {
+  connection.query("DELETE FROM Comments WHERE id=?", [commentId], (error, results, fields) => {
     if (error) {
       res.status(500).json({ "error": error.sqlMessage });
     } else {
       res.status(201).json({ message: 'Commentaire supprimée' });
     }
   });
-  connection.end();
 }
