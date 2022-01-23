ef8eed526a550de625dfa0b2c0856ac1b3afea91
etiqueludovic <etique.ludovic@gmail.com>
2022-01-23 20:07:43 +0100

    Update Message.model.ts

--- Frontend/src/app/models/Message.model.ts
+++ Frontend/src/app/models/Message.model.ts
@@ -3,9 +3,10 @@ export class Message {
 
     constructor(public title?: string, 
                 public content?: string,
-                public userId?: string,
+                public userId?: number,
                 public username?: string,
                 public id?: number,
                 public imageUrl?: string,
+                public image_profil?: string
                 ) {}
 }
