f6eb5ca098ecb874ba747575d52d9445843c3726
etiqueludovic <etique.ludovic@gmail.com>
2022-01-24 15:49:33 +0100

    Update messageRoute.js

--- Backend/routes/messageRoute.js
+++ Backend/routes/messageRoute.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-//const auth = require('../middleware/auth');
 const postCtrl = require('../controllers/messages');
 
 router.post('/addmessage', postCtrl.newMessages);
